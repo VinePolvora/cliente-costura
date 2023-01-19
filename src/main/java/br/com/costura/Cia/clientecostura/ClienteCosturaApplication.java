73bb97da50075afca2f3d4aee7cbe9b04c0d845e
unknown <santosvinepolvora@gmail.com>
2023-01-18 21:38:30 -0300

    inicia o projeto

---
+++ src/main/java/br/com/costura/Cia/clientecostura/ClienteCosturaApplication.java
@@ -0,0 +1,13 @@
+package br.com.costura.Cia.clientecostura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteCosturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteCosturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/costura/Cia/clientecostura/ClienteCosturaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.costura.Cia.clientecostura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteCosturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
