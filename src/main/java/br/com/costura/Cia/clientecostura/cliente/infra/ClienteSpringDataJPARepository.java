5c946c6abb1bbc14913ffe4464ab29d325cb2fd0
unknown <santosvinepolvora@gmail.com>
2023-01-23 00:28:10 -0300

    implementa cliente repository

---
+++ src/main/java/br/com/costura/Cia/clientecostura/cliente/infra/ClienteSpringDataJPARepository.java
@@ -0,0 +1,11 @@
+package br.com.costura.Cia.clientecostura.cliente.infra;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.costura.Cia.clientecostura.cliente.domain.Cliente;
+
+public interface ClienteSpringDataJPARepository extends JpaRepository<Cliente, UUID>{
+	
+}
