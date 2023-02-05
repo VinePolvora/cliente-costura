b529e0a9b55d6597f4ef712f3c0a44e209a8e8c8
unknown <santosvinepolvora@gmail.com>
2023-02-05 02:39:40 -0300

    implementa deleta

--- src/main/java/br/com/costura/Cia/clientecostura/cliente/application/repository/ClienteRepository.java
+++ src/main/java/br/com/costura/Cia/clientecostura/cliente/application/repository/ClienteRepository.java
@@ -9,4 +9,5 @@ public interface ClienteRepository {
 	Cliente salva(Cliente cliente);
 	List<Cliente> buscaTodosCleintes();
 	Cliente buscaCleinteAtravesId(UUID idCliente);
+	void deletaCliente(Cliente cliente);
 }
--- src/main/java/br/com/costura/Cia/clientecostura/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/costura/Cia/clientecostura/cliente/application/service/ClienteApplicationService.java
@@ -52,7 +52,7 @@ public class ClienteApplicationService implements ClienteService {
 	public void deletaClienteAtravesId(UUID idCliente) {
 		log.info("[inicia] ClienteApplicationService - deletaClienteAtravesId");
 		Cliente Cliente = clienteRepository.buscaCleinteAtravesId(idCliente);
-//		clienteRepository.deletaCliente(Cliente);
+		clienteRepository.deletaCliente(Cliente);
 		log.info("[finaliza] ClienteApplicationService - deletaClienteAtravesId");
 	}
 }
